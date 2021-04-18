97abc85a293c26ddd544e3c7bf0744d69dbcab96
NEMO DE SOUZA <53828046+nemodesouza@users.noreply.github.com>
2021-04-18 10:26:16 -0300

    livro atualização

--- livro.py
+++ livro.py
@@ -5,6 +5,15 @@ from capitulo import Capitulo
 class Livro:
     def __init__(self, codigo: int, titulo: str, ano: int, editora: Editora, autor: Autor, numero_capitulo: int, titulo_capitulo: str):
         # Criar todos os atributos, incluindo as listas
+        self.__codigo = codigo
+        self.__titulo = titulo
+        self.__ano = ano
+        self.__editora = editora
+        self.__autor = []
+        self.__numero_capitulo = numero_capitulo
+        self.__titulo_capitulo = titulo_capitulo
+        self.__capitulos = []
+
         # Incluir o primeiro autor e o primeiro capitulo nas respectivas listas
         ...
 
@@ -16,26 +25,70 @@ class Livro:
     def codigo(self, codigo):
         self.__codigo = codigo
 
-    ... Adicionar demais getters
-    
-    ... Adicionar demais setters
+    @property
+    def titulo(self):
+        return self.__titulo
+
+    @titulo.setter
+    def titulo(self, titulo):
+        self.__titulo = titulo
+
+    @property
+    def editora(self):
+        return self.__editora
+
+    @editora.setter
+    def editora(self, editora: Editora):
+        if isinstance(editora, Editora):
+            self.__editora = editora
+        else:
+            print('Insira uma editora correta')
+
+    @property
+    def autor(self):
+        return self.__autor
+
+    @autor.setter
+    def autor(self, autor: Autor):
+        if isinstance(autor, Autor):
+            self.__autor.append(autor)
     
 
     def incluir_autor(self, autor: Autor):
         #Nao esqueca de garantir que o objeto recebido pertence a classe Autor...
+        if isinstance(autor, Autor):
+
         # Nao permitir insercao de Autores duplicados!
-        ...
+            if autor in self.__autor:
+                self.__autor.append(autor)
+            else:
+                print('O(A) Autor(a) informado já está na lista')
 
     def excluir_autor(self, autor: Autor):
-        ...
+        if isinstance(autor, Autor):
+            if autor in self.__autor:
+                self.__autor.pop(autor)
+            else:
+                print('O(A) Autor(a) informado já inexiste na lista')
+
 
     def incluir_capitulo(self, numero: int, titulo: str):
-        ... Nao permitir insercao de Capitulos duplicados!
+        #... Nao permitir insercao de Capitulos duplicados!
+        novo_capitulo = Capitulo(numero, titulo)
+        if novo_capitulo in self.__capitulos:
+            self.__capitulos.append(novo_capitulo)
+        else:
+            print("O capítulo informado já está inserido")
 
     def excluir_capitulo(self, titulo: str):
-        ...
+
+        if isinstance(titulo, Capitulo.titulo):
+            self.__capitulos.pop(Capitulo.[titulo])
+
+        else:
+            print("O capítulo informado não pertence ao livro")
+
 
     def find_capitulo_by_titulo(self, titulo: str):
-        # Procura na lista de capitulos se existe um Capitulo com este titulo 
-        # Se encontrar, retorna o Capitulo encontrado
-        ...
+
+
